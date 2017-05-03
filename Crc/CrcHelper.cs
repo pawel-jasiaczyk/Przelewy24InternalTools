399c55fd3f9ff3f55015c9496168b0d064ffd01e
pawel-jasiaczyk <pawel.jasiaczyk@gmail.com>
2017-05-03 11:43:27 +0200

    First Commit

---
+++ Crc/CrcHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Przelewy24;
+
+namespace Przelewy24InrernalTools.Crc
+{
+    public class CrcHelper
+    {
+        public string TestedCrc { get; set; }
+        
+        public string P24_merchant_id { get; set; }
+        public string P24_session_id { get; set; }
+        public string P24_amount { get; set; }
+        public string P24_currency { get; set; }
+        public string CrcKey { get; set; }
+        public string TransactionId { get; set; }
+
+        public ParameterVariation[] Errors { get; private set; }
+
+        public CrcHelper()
+        {
+            Errors = new ParameterVariation[0];
+        }
+
+        //private string[] ParameterVariations(string parameter)
+        //{
+        //    List<string> variations = new List<string>();
+
+        //    variations.Add(parameter);
+        //    variations.Add(parameter + " ");
+        //    variations.Add(" " + parameter);
+        //    variations.Add("");
+        //    variations.Add(null);
+
+        //    return variations.ToArray();
+        //}
+
+        public bool FindBugInRegisterSign32()
+        {
+            bool result = false;
+            List<ParameterVariation> errors = new List<ParameterVariation> ();
+            List<Parameter> parameters = new List<Parameter> ();
+            
+            parameters.Add (new Parameter ("p24_session_id", this.P24_session_id));
+            parameters.Add (new Parameter ("p24_merchant_id", this.P24_merchant_id));
+            parameters.Add (new Parameter ("p24_amount", this.P24_amount);
+            parameters.Add (new Parameter ("p24_currency", this.P24_currency));
+            parameters.Add (new Parameter ("crckey", this.CrcKey));
+
+            ParameterVariation[][] variations = new ParameterVariation[5][];
+            for(int i = 0; i < 5; i++)
+            {
+                variations[i] = ParameterVariation.GetAllVariations(parameters[i]);
+            }
+            // ten fragment trzeba zapisać rekurencyjnie
+            for(int i =0; i < variations.Length; i++)
+            {
+                for(int j = 0; j < variations[i].Length; j++)
+                {
+                    string variationHash = Przelewy24.Przelewy24.
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Crc/ParameterVariation.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Przelewy24;
+
+namespace Przelewy24InrernalTools.Crc
+{
+    class ParameterVariation
+    {
+        #region Properties
+
+        public string ParameterName { get; private set; }
+        public VariationName Variation { get; private set; }
+        public string ParameterValue { get; private set; }
+        public string VariationValue { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        public ParameterVariation (Parameter parameter, VariationName variation)
+            : this (parameter.Name, parameter.Value, variation)
+        { }
+
+        public ParameterVariation (string name, string value, VariationName variation)
+        {
+            this.ParameterName = name;
+            this.ParameterValue = value;
+            this.Variation = variation;
+
+            switch (variation)
+            {
+                case (VariationName.omnited):
+                {
+                    this.VariationValue = null;
+                    break;
+                }
+                case (VariationName.plain):
+                {
+                    this.VariationValue = value;
+                    break;
+                }
+                case (VariationName.spaceAfter):
+                {
+                    this.VariationValue = value + " ";
+                    break;
+                }
+                case (VariationName.spaceBefore):
+                {
+                    this.VariationValue = " " + value;
+                    break;
+                }
+                case (VariationName.spaceBoth):
+                {
+                    this.VariationValue = " " + value + " ";
+                    break;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Static Methods
+
+        public static ParameterVariation[] GetAllVariations (Parameter parameter)
+        {
+            return GetAllVariations (parameter.Name, parameter.Value);
+        }
+
+        public static ParameterVariation[] GetAllVariations (string name, string value)
+        {
+            List<ParameterVariation> variations = new List<ParameterVariation> ();
+
+            for (int i = 0; i < 5; i++)
+            {
+                variations.Add (new ParameterVariation (name, value, (VariationName)i));
+            }
+
+            return variations.ToArray ();
+        }
+
+        #endregion
+    }
+}
